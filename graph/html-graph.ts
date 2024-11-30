be9073ee794d299b9d4ddf0e6b57aa8f6a465707
krypek <krypek@tuta.io>
2024-11-30 22:50:07 +0100

    Simplify graph outlier remover

--- graph/html-graph.ts
+++ graph/html-graph.ts
@@ -7,34 +7,16 @@ type Entry = (typeof dataPointsArr)[any]
     dataPointsArr.reverse()
 
     const dataPoints: Entry[] = []
-    for (let i = 0, j = 0; i < dataPointsArr.length; i++, j++) {
+    for (let i = 0; i < dataPointsArr.length; i++) {
         const e = dataPointsArr[i]
-
-        let avgLast: number
-        {
-            const firstI = -30
-            const lastI = 50
-            let offsetI = 100
-            const sliceArr = dataPoints.slice(i + offsetI + firstI, i).concat(dataPointsArr.slice(i, i + offsetI + lastI))
-            if (sliceArr.length == 0) {
-                avgLast = e.typedefData.classes
-            } else avgLast = sliceArr.reduce((acc: number, v: Entry) => v.typedefData.classes + acc, 0) / sliceArr.length
-        }
-        let avgFirst: number
-        {
-            const firstI = 15
-            const lastI = 1
-            let offsetI = -20
-            const sliceArr = dataPoints.slice(j + offsetI + firstI, j).concat(dataPointsArr.slice(i, i + offsetI + lastI))
-            if (sliceArr.length == 0) {
-                avgFirst = e.typedefData.classes
-            } else avgFirst = sliceArr.reduce((acc: number, v: Entry) => v.typedefData.classes + acc, 0) / sliceArr.length
-        }
-
-        if (e.typedefData.classes - avgFirst < 0 || e.typedefData.classes - avgLast > 0) {
-            j--
+        const le = dataPoints[dataPoints.length - 1]
+        if (!le) {
+            dataPoints.push(e)
             continue
         }
+
+        if (e.typedefData.classes < le.typedefData.classes) continue
+
         dataPoints.push(e)
     }
 
@@ -60,11 +42,6 @@ type Entry = (typeof dataPointsArr)[any]
                     data: dataPoints.map(e => e.typedefData.fields),
                     backgroundColor: 'purple',
                 },
-                // {
-                //     label: 'Average',
-                //     data: dataPoints.map(e => (e.typedefData.classes + e.typedefData.functions + e.typedefData.fields) / 3),
-                //     backgroundColor: 'red',
-                // },
             ],
         },
         options: {
